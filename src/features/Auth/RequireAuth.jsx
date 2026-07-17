49d767a3db7b474f4789a03d5cb07d696446d3a3
Vitaly <vitaly.sadikov1@yandex.ru>
2026-07-17 15:33:07 +0500

    fix bugs

--- src/features/Auth/RequireAuth.jsx
+++ src/features/Auth/RequireAuth.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 
 import { useAuthStore } from "../../shared/stores/authStore";
 
